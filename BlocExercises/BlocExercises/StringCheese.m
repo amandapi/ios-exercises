d5831ac5d23455cb095ab492a4b777c124001941
Amanda Pi <amandapi88@gmail.com>
2015-01-01 17:49:04 -0800

    StringCheese

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -10,28 +10,42 @@
 
 @implementation StringCheese
 
+
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *cheeseStatement = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    return(cheeseStatement);
 }
+
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
-}
+    NSString *nameWithoutCheese = cheeseName;
+    if ([cheeseName hasSuffix:@" cheese"])
+    {
+    NSRange cheeseRange = [cheeseName rangeOfString:@" cheese"
+                           options:NSCaseInsensitiveSearch];
+    NSString *nameWithoutSuffix = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+        return (nameWithoutSuffix);
+    }
+        else {return (cheeseName);
+        }}
 
+    
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        return [NSString stringWithFormat:@"1 cheese"];
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSString *cheeseString = [NSString stringWithFormat:@"cheeses"];
+        return [NSString stringWithFormat:@"%ld %@", (long)cheeseCount , cheeseString];
     }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
     
-    return nil;
 }
 
 @end
