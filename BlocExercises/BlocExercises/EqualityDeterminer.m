b4437e0b78d4c1a1734549623241e2d8341d5636
Amanda Pi <amandapi88@gmail.com>
2015-01-02 16:15:42 -0800

    EqualityDeterminer

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -10,19 +10,41 @@
 
 @implementation EqualityDeterminer
 
+
+
+
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    //wrong method1
+    //  if(string1 == string2)
+    //       {return(@"YES");}
+    //  else {return(@"NO");}
+    //
+    //wrong method2
+    //   return [string1 isTheSameAs: string2];
+   
+    return [string1 isEqual: string2];
 }
+
+
+
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    
+    return [number1 isEqual: number2];
 }
+
+
+
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    //wrong method 3
+    // return [integer1 > integer2];
+    
+    return integer1 > integer2;
+    
 }
 
 @end
