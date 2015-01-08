c05b7c90550bbe4014399b69ad8900de229af8eb
Amanda Pi <amandapi88@gmail.com>
2015-01-07 20:24:52 -0800

    StarTrekArrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -10,24 +10,56 @@
 
 @implementation StarTrekArrays
 
+
+
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    //String to Array
+    
+    NSArray *characterArray = [characterString componentsSeparatedByString:@";"];
+    
+    return characterArray;
 }
+
+
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    //Array to String
+    
+    NSString *characterString = [characterArray componentsJoinedByString:@";"];
+    
+    return characterString;
 }
+
+
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    //Sorting
+
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil  ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    NSArray *sortedCharacterArray = [characterArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+    
+    return sortedCharacterArray;
 }
+
+
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
-}
+    //Filtering
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+    
+    NSArray *filteredArray = [characterArray filteredArrayUsingPredicate:containsWorf];
 
+    if (filteredArray.count == 0) {
+        return NO;
+    } else {
+        return YES;
+    }
+
+}
 @end
