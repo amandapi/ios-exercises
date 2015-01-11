fef4befa484533a211727362155e151c031ec9d0
Amanda Pi <amandapi88@gmail.com>
2015-01-11 12:43:03 -0800

    NumberHandler.m

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -10,19 +10,54 @@
 
 @implementation NumberHandler
 
+
+
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    // unboxing
+    
+    int unboxNumber = [number intValue];
+    int twiceInt = (unboxNumber * 2);
+    return @(twiceInt);
+
 }
+
+
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    // adding entries
+    
+    NSInteger lowNumber = number;
+    NSInteger highNumber = otherNumber;
+    
+    if (lowNumber > highNumber) {
+        highNumber = number  ;
+        lowNumber = otherNumber;
+    }
+    
+    NSMutableArray *modifiableReturn = [[NSMutableArray alloc] init];
+    for (NSInteger i=lowNumber; i <= highNumber ; i++) {
+        [modifiableReturn addObject:@(i)];
+    }
+     return modifiableReturn;
+
 }
 
-- (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
+
+
+    - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    // sorting numbers and returning [0] entry
+        
+    // Create a mutable array from the given arrayOfNumbers
+        NSMutableArray *myMutableArray = [arrayOfNumbers mutableCopy];
+    // Use the sorting ascending method as described in Checkpoint:Arrays
+        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+        [myMutableArray sortUsingDescriptors:@[sortDescriptor]];
+    // Unbox the lowest number from NSNumber to Int using method described in Checkpoint:Simple Data Types and Objects
+        return [myMutableArray[0] intValue];
+        
 }
 
 @end
