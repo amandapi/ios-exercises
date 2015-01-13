eb291698bf09453adf93aa4b84af2414d3bc82c6
Amanda Pi <amandapi88@gmail.com>
2015-01-13 14:45:05 -0800

    StuffRememberer

--- BlocExercises/BlocExercises/StuffRememberer.m
+++ BlocExercises/BlocExercises/StuffRememberer.m
@@ -12,29 +12,45 @@
 
 - (void) rememberThisArrayForLater:(NSMutableArray *)arrayToRemember {
     /* WORK HERE */
+    // placed @property (nonatomic, strong) NSMutableArray *arrayToRemember; in StuffRememberer.h
+    self.arrayToRemember = arrayToRemember;
 }
+
+
 
 - (void) copyThisArrayForLater:(NSMutableArray *)arrayToCopy {
     /* WORK HERE */
+    // placed @property (nonatomic, copy) NSMutableArray *arrayToCopy; in StuffRememberer.h
+    self.arrayToCopy = arrayToCopy;
 }
 
+
+    
 - (void) rememberThisFloatForLater:(CGFloat)floatToRemember {
     /* WORK HERE */
+    // placed @property (nonatomic, assign) CGFloat floatToRemember; in StuffRememberer.h
+    self.floatToRemember = floatToRemember;
 }
+
+
 
 - (NSMutableArray *) arrayYouShouldRemember {
     /* WORK HERE */
-    return [@[] mutableCopy];
+    return self.arrayToRemember;
 }
 
+    
+    
 - (NSMutableArray *) arrayYouShouldCopy {
     /* WORK HERE */
-    return [@[] mutableCopy];
+    return self.arrayToCopy;
 }
+
+    
 
 - (CGFloat) floatYouShouldRemember {
     /* WORK HERE */
-    return 0.0f;
+    return self.floatToRemember;
 }
 
 @end
