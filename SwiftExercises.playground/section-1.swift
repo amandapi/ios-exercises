35ce3d406d3105d79be0049c08df81e8af352172
Amanda Pi <amandapi88@gmail.com>
2015-01-18 13:45:43 -0800

    SwiftExercises.playground/section-1.swift

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -13,6 +13,11 @@ func favoriteCheeseStringWithCheese(cheese: String) -> String {
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
 // Make fullSentence say "My favorite cheese is cheddar."
+let cheeseName = "My favorite cheese is "
+let fullStop = "."
+cheeseName + fullSentence + fullStop
+
+
 
 /*
 
@@ -23,10 +28,21 @@ Arrays & Dictionaries
 let numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+let nextArray = [5]
+numberArray + nextArray
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+
+
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+"This dictionary contains \(numberDictionary.count) items."
+// changed "let" to "var" in line 36
+numberDictionary.updateValue("five", forKey:5)
+"This dictionary contains \(numberDictionary.count) items."
+numberDictionary
+
+
 
 /*
 
@@ -36,9 +52,18 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in 1...10  {
+    println(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+
+for i in 1..<11  {
+    println(i)
+}
+
+
 
 let worf = [
     "name": "Worf",
@@ -58,12 +83,17 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+
+var drinkWorf = worf ["favorite drink"]
+var drinkPicard = picard ["favorite drink"]
+return [drinkWorf! , drinkPicard!]
+
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 
-favoriteDrinks
+(favoriteDrinks.count)
+
 
 /*
 
@@ -75,9 +105,11 @@ Functions
 
 let strings = ["milk", "eggs", "bread", "challah"]
 
-// WORK HERE - make your function and pass `strings` in
+// WORK HERE
+let semicolonString = join(";", strings)
+// another method:
+let delimited = ";".join(strings)
 
-let expectedOutput = "milk;eggs;bread;challah"
 
 /*
 
@@ -85,7 +117,17 @@ Closures
 
 */
 
-let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
+var cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+// changed "let" to "var" in line 115
+(cerealArray.count)
+cerealArray.sort { $0 < $1 }
+cerealArray
+
+
+
+
+
+
